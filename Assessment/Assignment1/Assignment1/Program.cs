a2c1968a951afb3c2ed3e48649cf726633316ffb
anushakn48374 <143707420+anushakn48374@users.noreply.github.com>
2024-07-28 20:38:44 +0530

    Assignment1

---
+++ Assessment/Assignment1/Assignment1/Program.cs
@@ -0,0 +1,69 @@
+﻿using static ConsoleApp2.simpleCalculator;
+
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+public interface Icalcii
+{
+    int Add(int x, int y);
+
+    int Subtract(int x, int y);
+
+    int Multiply(int x, int y);
+
+    float Divide(int x, int y);
+}
+
+namespace ConsoleApp2
+{
+    public class simpleCalculator : Icalcii
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int Subtract(int x, int y)
+        {
+
+            return x - y;
+
+        }
+        public int Multiply(int x, int y)
+        {
+
+            return x * y;
+
+        }
+        public float Divide(int x, int y)
+
+        {
+            return x / y;
+        }
+    }
+   
+}
+public class Program
+
+{
+
+    private static void Main(string[] args)
+    {
+
+        Icalcii calc = new simpleCalculator();
+
+        Console.WriteLine(calc.Add(5, 6));
+
+        Console.WriteLine(calc.Subtract(5, 6));
+
+        Console.WriteLine(calc.Multiply(5, 6));
+
+        Console.WriteLine(calc.Divide(5, 6));
+    }
+}
---
+++ Assessment/Assignment1/Assignment1Tests/simplecalculatorTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Tests
+{
+    [TestClass()]
+    public class simplecalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Icalcii calc=new simplecalculator();
+            int res = calc.Add(5, 5);
+            Assert.AreEqual(10,res);
+        }
+        public void SubtractTest()
+        {
+            Icalcii calc = new simplecalculator();
+            int res = calc.Subtract(5, 5);
+            Assert.AreEqual(0, res);
+        }
+        public void MultiplyTest()
+        {
+            Icalcii calc = new simplecalculator();
+            int res = calc.Multiply(5, 5);
+            Assert.AreEqual(25, res);
+        }
+        public void DivideTest()
+        {
+            Icalcii calc = new simplecalculator();
+            int res = calc.Divide(5, 5);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
